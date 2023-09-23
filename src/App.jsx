c9964dccb50ddad5c8e71b3d81db83387ad12a89
이해연 <dlgodus21@gmail.com>
2023-09-23 07:20:04 +0000

    Feat : contentView 아코디언 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,40 @@
 import React, { useState } from 'react';
+
+
+export default App;
+
 import { EmptyView, Header, CustomModal } from '@/components';
 import styles from './App.module.scss';
 import ContentView from './components/ContentView.jsx/ContentView';
 
 function App() {
-	
+
 	const [isOpen, setIsOpen] = useState(false);
 	
 	const toggle = () => {
    		setIsOpen(!isOpen);
   	};
 	
+    
 	return (
 		<div className={styles.App}>
-			<Header toggleModal={toggle}/>
+            <Header toggleModal={toggle}/>
 			<main className={styles.main}>
-				{
+				<ContentView/>
+
+            	{/*
 					localStorage.length != 0?
 					<ContentView/>
 					:
 					<EmptyView />
-				}
+				*/}
+            
 				<CustomModal isOpen={isOpen} toggle={toggle}/>
-				<EmptyView  />
 			</main>
 		</div>
 	);
 }
-
-export default App;
+            
+            
+            
+            
--- src/components/ContentView.jsx/ContentView.jsx
+++ src/components/ContentView.jsx/ContentView.jsx
@@ -2,51 +2,260 @@ import cn from 'classnames';
 
 import { Card } from '@/components';
 
-import { Typography } from '@goorm-dev/gds-challenge';
+import { Typography, Collapse} from '@goorm-dev/gds-challenge';
 
 // import styles from '../EmptyView/EmptyView.module.scss';
 import { useEffect, useState } from 'react';
 
 const ContentView = () => {
     const [items, setItems] = useState([]);
-    useEffect(() => {
-      for (let i = window.localStorage.length-1; i >= 0; i++) {
-        const key = window.localStorage.key(i);
-        const value = window.localStorage.getItem(key);
-        setItems((prevItems) => [...prevItems, value]);
-        // console.log(i, key, value, items);
-      }
-    }, []);
+    const [isClicked, setIsClicked] = useState(false);
+    
+    const toggle = () => {
+        setIsClicked((current) => !current)
+    }
+    
+  const [openCollapseIndex, setOpenCollapseIndex] = useState(-1);
+
+  const toggleCollapse = (index) => {
+    if (index === openCollapseIndex) {
+      // Clicked on the already open Collapse, close it
+      setOpenCollapseIndex(-1);
+    } else {
+      setOpenCollapseIndex(index);
+    }
+  };
 
     console.log(items);
 
+    const dummy = [
+        {
+            key: '1',
+            name: '이해연',
+            phoneNumber: '010-9227-5669',
+            email: 'seahaeyeon@naver.com',
+            question1: '1번답',
+            question2: '2번답',
+            question2_1: '2_1답',
+            question2_2: '2_2답',
+            question3: '3답',
+            question4: '4답'
+        },
+        {
+            key: '2',
+            name: '이해준',
+            phoneNumber: '010-9227-5669',
+            email: 'seahaeyeon@naver.com',
+            question1: '1번답',
+            question2: '2번답',
+            question2_1: '2_1답',
+            question2_2: '2_2답',
+            question3: '3답',
+            question4: '4답'
+        },
+        {
+            key: '2',
+            name: '이해준',
+            phoneNumber: '010-9227-5669',
+            email: 'seahaeyeon@naver.com',
+            question1: '1번답',
+            question2: '2번답',
+            question2_1: '2_1답',
+            question2_2: '2_2답',
+            question3: '3답',
+            question4: '4답'
+        },
+        {
+            key: '2',
+            name: '이해준',
+            phoneNumber: '010-9227-5669',
+            email: 'seahaeyeon@naver.com',
+            question1: '1번답',
+            question2: '2번답',
+            question2_1: '2_1답',
+            question2_2: '2_2답',
+            question3: '3답',
+            question4: '4답'
+        },
+        {
+            key: '2',
+            name: '이해준',
+            phoneNumber: '010-9227-5669',
+            email: 'seahaeyeon@naver.com',
+            question1: '1번답',
+            question2: '2번답',
+            question2_1: '2_1답',
+            question2_2: '2_2답',
+            question3: '3답',
+            question4: '4답'
+        },
+        {
+            key: '2',
+            name: '이해준',
+            phoneNumber: '010-9227-5669',
+            email: 'seahaeyeon@naver.com',
+            question1: '1번답',
+            question2: '2번답',
+            question2_1: '2_1답',
+            question2_2: '2_2답',
+            question3: '3답',
+            question4: '4답'
+        },
+        {
+            key: '2',
+            name: '이해준',
+            phoneNumber: '010-9227-5669',
+            email: 'seahaeyeon@naver.com',
+            question1: '1번답',
+            question2: '2번답',
+            question2_1: '2_1답',
+            question2_2: '2_2답',
+            question3: '3답',
+            question4: '4답'
+        },
+        {
+            key: '2',
+            name: '이해준',
+            phoneNumber: '010-9227-5669',
+            email: 'seahaeyeon@naver.com',
+            question1: '1번답',
+            question2: '2번답',
+            question2_1: '2_1답',
+            question2_2: '2_2답',
+            question3: '3답',
+            question4: '4답'
+        },
+        {
+            key: '2',
+            name: '이해준',
+            phoneNumber: '010-9227-5669',
+            email: 'seahaeyeon@naver.com',
+            question1: '1번답',
+            question2: '2번답',
+            question2_1: '2_1답',
+            question2_2: '2_2답',
+            question3: '3답',
+            question4: '4답'
+        },
+        {
+            key: '2',
+            name: '이해준',
+            phoneNumber: '010-9227-5669',
+            email: 'seahaeyeon@naver.com',
+            question1: '1번답',
+            question2: '2번답',
+            question2_1: '2_1답',
+            question2_2: '2_2답',
+            question3: '3답',
+            question4: '4답'
+        },
+        {
+            key: '2',
+            name: '이해준',
+            phoneNumber: '010-9227-5669',
+            email: 'seahaeyeon@naver.com',
+            question1: '1번답',
+            question2: '2번답',
+            question2_1: '2_1답',
+            question2_2: '2_2답',
+            question3: '3답',
+            question4: '4답'
+        },
+        {
+            key: '2',
+            name: '이해준',
+            phoneNumber: '010-9227-5669',
+            email: 'seahaeyeon@naver.com',
+            question1: '1번답',
+            question2: '2번답',
+            question2_1: '2_1답',
+            question2_2: '2_2답',
+            question3: '3답',
+            question4: '4답'
+        },
+        {
+            key: '2',
+            name: '이해준',
+            phoneNumber: '010-9227-5669',
+            email: 'seahaeyeon@naver.com',
+            question1: '1번답',
+            question2: '2번답',
+            question2_1: '2_1답',
+            question2_2: '2_2답',
+            question3: '3답',
+            question4: '4답'
+        },
+        {
+            key: '2',
+            name: '이해준',
+            phoneNumber: '010-9227-5669',
+            email: 'seahaeyeon@naver.com',
+            question1: '1번답',
+            question2: '2번답',
+            question2_1: '2_1답',
+            question2_2: '2_2답',
+            question3: '3답',
+            question4: '4답'
+        },
+        {
+            key: '2',
+            name: '이해준',
+            phoneNumber: '010-9227-5669',
+            email: 'seahaeyeon@naver.com',
+            question1: '1번답',
+            question2: '2번답',
+            question2_1: '2_1답',
+            question2_2: '2_2답',
+            question3: '3답',
+            question4: '4답'
+        },
+        {
+            key: '2',
+            name: '이해준',
+            phoneNumber: '010-9227-5669',
+            email: 'seahaeyeon@naver.com',
+            question1: '1번답',
+            question2: '2번답',
+            question2_1: '2_1답',
+            question2_2: '2_2답',
+            question3: '3답',
+            question4: '4답'
+        },
+        
+    ];
+    
+    // useEffect(() => {
+    //   for (let i = dummy.length - 1; i >= 0; i--) {
+    //     setItems((current) => [...current, dummy[i]]);
+    //   }
+    // }, []);
+    // alert(items)
+    
 	return (
-		<Card center padding="none"  children={items}>
-			{
-                <>
-
-                <div style={{padding: '10px'}}>
-                <Typography>참여한 응답자</Typography> <Typography  color="secondary">{localStorage.length}</Typography>
-                </div>
-                <hr></hr>
-                <div style={{overflow:'auto', maxHeight:'720px'}}>
-                {
-                    items.map((item, index)=>(
-                        <>
-                        {index !== 0 ? <hr></hr> : <div style={{height:'12px'}}></div>}
-                        <div key={index}>
-                            참여자 {index}. {item}
-                        </div>
-                        </>
-                    ))
-
-                }
-                </div>
-				<Typography color="hint" token="paragraph-sm">
-				</Typography>
-				</>
-			}
+        <>
+		<Card center padding="md">
+            <Typography>참여한 응답자</Typography> <Typography  color="secondary">{localStorage.length}</Typography>
 		</Card>
+        <div style={{ overflow: 'auto', maxHeight: '720px' }}>
+        {
+            dummy.slice().reverse().map((item, index) => (
+                <>
+                    <div style={{padding: '1rem 1.5rem', width: '100%',  border: '1px solid var(--gray-300)', lineHeight: '22px'}} key={index} onClick={() => toggleCollapse(index)}>
+                      {dummy.length - index} {item.name}
+                    </div>
+                    <Collapse isOpen={index === openCollapseIndex}>
+                        <Card center type='content' padding="none"  children={items}>
+                            1
+                        </Card>
+                        <Card center type='content' padding="none"  children={items}>
+                            1
+                        </Card>
+                     </Collapse>
+                </>
+            ))
+        }
+        </div>
+        </>
 	);
 };
 
