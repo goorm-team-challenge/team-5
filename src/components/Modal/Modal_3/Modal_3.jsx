a04005b7e6cf55d345b77e3dd36f67814101eafb
Judgement9882 <kjy6135@inu.ac.kr>
2023-09-23 06:49:58 +0000

    Modify : Modal_3 change inputs to buttons

--- src/components/Modal/Modal_3/Modal_3.jsx
+++ src/components/Modal/Modal_3/Modal_3.jsx
@@ -1,4 +1,4 @@
-import { Typography, Input} from '@goorm-dev/gds-challenge';
+import { Typography, Button} from '@goorm-dev/gds-challenge';
 
 function Modal_3() {
 	return (
@@ -9,10 +9,10 @@ function Modal_3() {
 			<br></br><br></br>
 			<br></br>
 			
-			<Input type="text" maxlength="28" style={{ width: '100%', marginTop: '10px' }} placeholder="1. (예시) 정해진 시간 내에 오프라인 팀 챌린지 과제를 수행"></Input>
-			<Input type="text" maxlength="28" style={{ width: '100%', marginTop: '10px' }} placeholder="2."></Input>
-			<Input type="text" maxlength="28" style={{ width: '100%', marginTop: '10px' }} placeholder="3."></Input>
-			<Input type="text" maxlength="28" style={{ width: '100%', marginTop: '10px' }} placeholder="4."></Input>
+			<Button content="start" color="basic" maxlength="28" style={{ width: '100%', marginTop: '10px' }}>1. 정해진 시간 내에 오프라인 팀 챌린지 과제를 수행</Button>
+			<Button content="start" color="basic" maxlength="28" style={{ width: '100%', marginTop: '10px' }}>2. 팀원들과 협업을 하는 경험</Button>
+			<Button content="start" color="basic" maxlength="28" style={{ width: '100%', marginTop: '10px' }}>3. CTO님의 명강의</Button>
+			<Button content="start" color="basic" maxlength="28" style={{ width: '100%', marginTop: '10px' }}>4. 맛있는 점심식사</Button>
 		</div>
 	);
 }
