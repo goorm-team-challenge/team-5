ca22dd44d55a6152418885f4abe4bb20e4578676
YoonsikOh5 <skyland310@naver.com>
2023-09-23 06:19:02 +0000

    Feat : indicater 클릭시 이동 및 다음 버튼 클릭시 이동

--- src/components/Modal/CustomModal.jsx
+++ src/components/Modal/CustomModal.jsx
@@ -1,6 +1,18 @@
-import { Modal, Button } from '@goorm-dev/gds-challenge';
+import React, { useState } from 'react';
+import { CarouselIndicators, Modal, Button } from '@goorm-dev/gds-challenge';
 
 function CustomModal({isOpen, toggle}) {
+	
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const nextPage = () => {
+    if (currentPage < 4) {
+      setCurrentPage(currentPage + 1);
+    } else {
+      toggle(); // 모달 닫기
+    }
+  };
+	
 	return (
 		<div>
    	  	  <Modal isOpen={isOpen} toggle={toggle}>
@@ -10,8 +22,9 @@ function CustomModal({isOpen, toggle}) {
           <Modal.Body>
             컨텐츠를 넣어주세요.
           </Modal.Body>
-          <Modal.Footer>
-            <Button>다음</Button>
+          <Modal.Footer between>
+		    <CarouselIndicators length={4} activeIndex={currentPage - 1} onClickWithIndex={(index) => setCurrentPage(index + 1)}></CarouselIndicators>
+            <Button onClick={nextPage}>다음</Button>
           </Modal.Footer>
    		 </Modal>
 		</div>
